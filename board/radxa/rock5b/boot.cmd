dc8a37a589f4b7a8715c83867fc0c3401b78a9c8
Niklas Cassel <niklas.cassel@wdc.com>
2023-10-18 12:46:18 +0200

    board/radxa/rock5b: there is no need to specify rootfstype
    The ext4 rootfs is mounted as ext4 perfectly fine without rootfstype
    specified on the kernel command line.
    
    In order to keep the kernel command line clean, remove the superfluous
    kernel parameter.
    
    Signed-off-by: Niklas Cassel <niklas.cassel@wdc.com>

--- board/radxa/rock5b/boot.cmd
+++ board/radxa/rock5b/boot.cmd
@@ -1,3 +1,3 @@
-setenv bootargs root=PARTLABEL=rootfs rootfstype=ext4 clkin_hz=(25000000) earlycon clk_ignore_unused earlyprintk console=ttyS2,1500000n8 rootwait
+setenv bootargs root=PARTLABEL=rootfs clkin_hz=(25000000) earlycon clk_ignore_unused earlyprintk console=ttyS2,1500000n8 rootwait
 fatload mmc 1:1 ${loadaddr} image.itb
 bootm ${loadaddr}

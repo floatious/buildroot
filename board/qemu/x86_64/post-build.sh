14da5b861dde7d0b9e0b63dcea94a6b73fce68ab
Niklas Cassel <cassel@kernel.org>
2025-01-28 14:31:34 +0100

    automatic login
    Signed-off-by: Niklas Cassel <cassel@kernel.org>

--- board/qemu/x86_64/post-build.sh
+++ board/qemu/x86_64/post-build.sh
@@ -9,3 +9,9 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 	sed -i '/GENERIC_SERIAL/a\
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # QEMU graphical window' ${TARGET_DIR}/etc/inittab
 fi
+
+# Automatic login
+if [ -e ${TARGET_DIR}/etc/inittab ]; then
+    grep -qE '^console::respawn:-/bin/sh' ${TARGET_DIR}/etc/inittab || \
+	sed -i 's,console::respawn:/sbin/getty -L  console 0 vt100 # GENERIC_SERIAL,console::respawn:-/bin/sh,g' ${TARGET_DIR}/etc/inittab
+fi
